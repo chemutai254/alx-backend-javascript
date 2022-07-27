264e5420a71a7d077f9f46c8203b863d4e19d98f
chemutai254 <nancy.chemutai254>
2022-07-27 11:21:30 -0700

    Correrection

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
-export default function handleResponseFromAPI(promise) {
- return promise
-   .then(() => ({
-     status: 200,
-     body: 'success',
- }));
-     .catch(() => Error())
-     .finally(() => console.warn('Got a response from the API')); 
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
 
+export default handleResponseFromAPI;
