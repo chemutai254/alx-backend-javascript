80e71938d5a5790634db9d1252a784344022f7e9
chemutai254 <nancy.chemutai254>
2022-07-28 08:02:30 -0700

    classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,7 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const room = new ClassRoom(19);
-  const room1 = new ClassRoom(20);
-  const room2 = new ClassRoom(34);
-  console.log(room, room1, room2);
+export default function initializeRooms() {
+  for (const room of this._maxStudentsSize) {
+    yield room;
+  }
 }
-
-export default initializeRooms;
