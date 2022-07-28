88016b415f92f6574b706b5b7c33d17d0bd3377c
chemutai254 <nancy.chemutai254>
2022-07-28 14:16:13 -0700

    Getters and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,39 +1,40 @@
 export default class HolbertonCourse {
- constructor (name, length, students) {
-   this.name = name;
-   this.length = length;
-   this.students = students;
- } 
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-   get name() {
-        return this._name;
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string' && name === '') {
+      throw new TypeError('The name must be a string and cannot be empty');
     }
-   get length() {
-        return this._length;
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number' && length === '') {
+      throw new TypeError('The name must be a number and cannot be empty');
     }
-    get students() {
-        return this._students;
-    } 
-    set name(name) {
-        name = name.trim();
-        if (typeof name != 'string' && name === '') {
-            throw 'The name must be a string and cannot be empty';
-        }
-        this._name = name;
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students) && students.every((s) => typeof s === 'string' && students !== '')) {
+      this._students = students;
     }
-}
-    set length(length) {
-        if (typeof length != 'number' && length === '') {
-            throw 'The name must be a number and cannot be empty';
-        }
-        this._length = length;
-    }
-}
-    set students(students) {
-        students = students.trim();
-        if (Array.isArray(students) && students.every((s) => typeof s === 'string' && students != '') {
-           this._students = students;
-        }
-        throw 'The name cannot be empty';  
-    }
+    throw new TypeError('The name must be a string and  cannot be empty');
+  }
 }
