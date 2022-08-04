057b0b0c71980aa3b1b6efebe249d3c2555d1a2f
Nancy Chemutai <88308985+chemutai254@users.noreply.github.com>
2022-08-04 16:32:47 +0300

    Delete 0x00-python_variable_annotations directory

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""type-annotated function add"""
-
-
-def add(a: float, b: float) -> float:
-    """Adds functions and returns sum"""
-    return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""type-annotated function concat"""
-
-
-def concat(str1: str, str2: str) -> str:
-    """Returns concatenated string"""
-    return str1 + str2
